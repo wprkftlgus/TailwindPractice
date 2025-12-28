b13536afcc54e618c02331b041a546028c770895
wprkftlgus <wprkftlgus@naver.com>
2025-12-28 16:47:16 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+import picture1 from "../src/assets/1.jpg";
+import picture2 from "../src/assets/2.jpg";
+import picture3 from "../src/assets/3.jpg";
+import picture4 from "../src/assets/4.jpg";
+import picture5 from "../src/assets/5.jpg";
+
 export default function App() {
+  const images: string[] = [
+    picture1,picture2,picture3,picture4,picture5
+  ];
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const nextSlide = () => {
+    setCurrentIndex((prev) => prev === images.length -1 ? 0 : prev + 1)
+  }
+  const prevSlide = () => {
+    setCurrentIndex((prev) => prev === 0 ? images.length - 1 : prev - 1)
+  }
+  console.log(currentIndex);
   return (
-    <div className="md:text-5xl lg:text-6xl mb-4 py-4 py-2 text-4xl bg-amber-600 rounded-3xl flex justify-center hover:bg-amber-400 hover:text-2xl " >
-     <div className="">hello!</div>
-     <div className="">My</div>
-     <div className="">Name</div>
+    <div>
+      <div>Get the highlights.</div>
+      <div className="w-200 overflow-hidden">
+        <div 
+        className="flex transition-transform duration-500 ease-in-out"
+        style={{ transform: `translateX(-${currentIndex * 100}%)`}}>
+        {images.map((img, index) => (
+          <img key={index} src={img}  className="w-full flex-shrink-0"/>
+        ))}
+        </div>
+        
+      </div>
+      <button className="w-20 h-20 bg-amber-700" onClick={prevSlide}>prev</button>
+      <button className="w-20 h-20 bg-amber-700" onClick={nextSlide}>next</button>
     </div>
   )
 }
