b8171c9369abd4a0936090b756528db24db566f9
wprkftlgus <wprkftlgus@naver.com>
2025-12-28 20:38:08 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import picture3 from "../src/assets/3.jpg";
 import picture4 from "../src/assets/4.jpg";
 import picture5 from "../src/assets/5.jpg";
 import women from "../src/assets/women.jpg";
+import ring from "../src/assets/ring.png";
+import react from "../src/assets/react.png"
+import health from "../src/assets/health.png"
 
 export default function App() {
   const images: string[] = [
@@ -61,9 +64,63 @@ export default function App() {
       </div>
       </div>
       </div>
-      <div className="w-full backdrop-[blur(40px)_brightness(60%)bg-white/30] " style={{backgroundImage: `url(${women})`, height: 1000, 
+      <div className="w-full backdrop-[blur(40px)_brightness(60%)bg-white/30] " style={{backgroundImage: `url(${women})`, height: 1100, 
       backgroundSize: "cover", backgroundPosition: "center"}}>
-      
+      <div className=" max-w-3xl mx-auto pt-20">
+      <div className="text-white font-bold">
+        <div className=" text-4xl max-w-100">Get notified of chronic high blood pressure.</div>
+        <div className="mt-2.5 max-w-150">Hypertension, or high blood pressure, impacts over 1.3 billion adults worldwide and is a leading cause of heart attack, stroke and kidney disease. The condition frequently goes undiagnosed because it often has no symptoms — and even during a doctor’s visit, with a single measurement it can be easily missed.</div>
+      </div>
+      <div className="flex gap-5 text-white text-xs mt-12">
+        <div className="max-w-70">
+        <div 
+        style={{backgroundImage: `url(${ring})`, width: 25, height:35, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
+        <div>Receive hypertension notifications, just by wearing your watch. Series 11 can notify you if it identifies patterns of hypertension.1 How? The optical sensor provides data to an algorithm that can detect potential hypertension by analysing how your blood vessels respond to beats of the heart over 30‑day periods.</div>
+        </div>
+        <div className="max-w-70">
+        <div style={{backgroundImage: `url(${react})`, width: 25, height:35, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
+        <div>Ground­breaking insights. Grounded in science. We developed hypertension notifications using advanced machine learning methods and a series of studies, totalling over 100,000 participants. The feature was then validated in a clinical study.</div>
+        </div>
+        <div className="max-w-70">
+        <div style={{backgroundImage: `url(${health})`, width: 25, height:40, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
+        <div>Set up a blood pressure log. If you receive a hypertension notification and have access to a blood pressure cuff, you can track your blood pressure in the Health app on iPhone to create a report that will lead to more meaningful conversations with your healthcare provider.</div>
+        </div>
+      </div>
+      </div>
+
+      </div>
+      <div className=" text-4xl font-bold">Apple Watch</div>
+      <div className="flex">
+      <div className="mr-12 font-bold text-xl cursor-pointer">
+        <div className=" font-light text-base cursor-auto text-[#6e6e73]">Explore Watch</div>
+        <div>Explore All Apple Watch</div>
+        <div>Apple Watch Series 11</div>
+        <div>Apple Watch SE 3</div>
+        <div>Apple Watch Ultra 3</div>
+        <div>Apple Watch Nike</div>
+        <div>Apple Watch Hermès</div>
+      </div>
+      <div className="mr-7 font-bold cursor-pointer">
+        <div className=" font-light cursor-auto text-[#6e6e73]">Shop Watch</div>
+        <div>Shop Apple Watch</div>
+        <div>Apple Watch Straps</div>
+        <div>Apple Watch Accessories</div>
+        <div>Apple Trade In</div>
+        <div>Financing</div>
+        <div>Personal Setup</div>
+      </div>
+      <div className="font-bold cursor-pointer">
+        <div className=" font-light cursor-auto text-[#6e6e73]">More from Watch</div>
+        <div>Apple Watch Support</div>
+        <div>AppleCare</div>
+        <div>watchOS 26</div>
+        <div>Apps by Apple</div>
+        <div>Apple Fitness+</div>
+      </div>
+      </div>
+      <div className="cursor-pointer">
+      <div className="mt-3.5 mb-1 ">Compare Watch</div>
+      <div>Why Apple Watch</div>
       </div>
     </div>
   )
