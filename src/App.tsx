93357a8fa85ab4301d5d7f029e30dd411b1a52ca
wprkftlgus <wprkftlgus@naver.com>
2025-12-28 11:55:18 +0900

    commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-5xl font-extrabold text-pink-500">
+        Tailwind OK 🚀
+      </h1>
+    </div>
+  )
+}
