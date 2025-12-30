4df130f80e2116b452ef8184e703606d448f0dae
wprkftlgus <wprkftlgus@naver.com>
2025-12-30 14:19:16 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,16 @@ import women from "../src/assets/women.jpg";
 import ring from "../src/assets/ring.png";
 import react from "../src/assets/react.png"
 import health from "../src/assets/health.png"
+import apple1 from "../src/assets/apple1.jpg"
+import apple2 from "../src/assets/apple2.jpg"
+import apple3 from "../src/assets/apple3.jpg"
+import apple4 from "../src/assets/apple4.jpg"
+import apple5 from "../src/assets/apple5.jpg"
+import apple6 from "../src/assets/apple6.jpg"
 import watch1 from "../src/assets/watch1.jpg"
-import watch2 from "../src/assets/watch2.jpg"
+import watch2 from "../src/assets/watch2.png"
+import run from "../src/assets/run.png"
+import clock from "../src/assets/clock.png"
 
 export default function App() {
   const images: string[] = [
@@ -29,25 +37,38 @@ export default function App() {
     return() => clearTimeout(timer);
   }, [currentIndex]);
 
-  const trigger = useRef<HTMLDivElement>(null);
+  const trigger1 = useRef<HTMLDivElement>(null);
+  const trigger2 = useRef<HTMLDivElement>(null);
   const bg = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const triggerRef = trigger.current;
+    const triggerRef1 = trigger1.current;
+    const triggerRef2 = trigger2.current;
     const bgRef = bg.current;
 
-    if (!triggerRef || !bgRef) return;
+    if (!triggerRef1 || !triggerRef2 || !bgRef) return;
 
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const entry = entries[0];
+    const observer1 = new IntersectionObserver(
+      (entries1) => {
+        const entry1 = entries1[0];
 
-        if (entry.isIntersecting) {
+        if (entry1.isIntersecting) {
           bgRef.classList.add("bg-fixed", "bg-blur");
-        } else{bgRef.classList.remove("bg-fixed", "bg-blur")}
+        } 
       }, { threshold: 0.5 }
     );
-    observer.observe(triggerRef);
+    const observer2 = new IntersectionObserver(
+      (entries2) => {
+        const entry2 = entries2[0];
+
+        if (entry2.isIntersecting) {
+          bgRef.classList.remove("bg-fixed", "bg-blur")
+        }
+      }, { threshold: 0.5 }
+    );
+    observer1.observe(triggerRef1);
+    observer2.observe(triggerRef2);
+
   },[]);
 
   return (
@@ -78,11 +99,11 @@ export default function App() {
       </div>
       <button className="w-20 h-20 bg-amber-700" onClick={prevSlide}>prev</button>
       <button className="w-20 h-20 bg-amber-700" onClick={nextSlide}>next</button>
-      <div className="relative w-full h-[1200px]">
-      <div ref={bg} className="absolute inset-0" style={{backgroundImage: `url(${women})`, 
+      <div className="relative w-full h-[2000px]">
+      <div ref={bg} className="absolute inset-0 transition-all duration-2000" style={{backgroundImage: `url(${women})`, 
       backgroundSize: "cover", backgroundPosition: "center"}}>
       </div>
-      <div ref={trigger} className="relative">
+      <div className="relative">
       <div className=" mx-auto w-300 pt-30">
       <div className="text-white font-bold max-w-[525px] ml-140">
       <div className="text-[#50c1fc] font-medium">Health</div> 
@@ -92,7 +113,7 @@ export default function App() {
       </div>
       <div className=" max-w-3xl mx-auto pt-70">
       <div className="text-white font-bold">
-        <div className=" text-4xl max-w-100">Get notified of chronic high blood pressure.</div>
+        <div ref={trigger1} className=" text-4xl max-w-100">Get notified of chronic high blood pressure.</div>
         <div className="mt-2.5 max-w-150">Hypertension, or high blood pressure, impacts over 1.3 billion adults worldwide and is a leading cause of heart attack, stroke and kidney disease. The condition frequently goes undiagnosed because it often has no symptoms — and even during a doctor’s visit, with a single measurement it can be easily missed.</div>
       </div>
       <div className="flex gap-5 text-white text-xs mt-12">
@@ -111,12 +132,71 @@ export default function App() {
         </div>
       </div>
       </div>
+      <div className="text-white">
+      <div className="text-2xl pt-30 mb-5">More features to empower your health.</div>
+      <div className="flex w-1000 gap-3">
+      <div className="flex flex-col">
+      <div className=" w-110 h-75 relative">
+      <div className="inset-0 absolute" style={{backgroundImage: `url(${apple1})`, borderRadius: 20, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
+      </div>
+      <div className="text-xs max-w-90 p-4">Wake up to your sleep score. Sleep quality is influenced by factors such as sleep duration, bed­time consistency, how often you wake up and time spent in each sleep stage. Sleep score analyses these factors every night and provides a classification and a score. You’ll see how the score is calculated, so you can understand the quality of your sleep and learn how to make it more restorative.</div>
+      </div>
+      <div className="flex flex-col">
+      <div className=" w-110 h-75 relative">
+      <div className="inset-0 absolute" style={{backgroundImage: `url(${apple2})`, borderRadius: 20, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
+      </div>
+      <div className="text-xs max-w-90 p-4">Keep an eye on sleep apnoea. Sleep apnoea is a condition where people experience repeated interruptions in their regular respiratory pattern during sleep. If untreated, it can lead to increased risk of hypertension, type 2 diabetes, cardiac issues and more. Series 11 can monitor for breathing disturbances over time and notify you of possible sleep apnoea.11</div>
+      </div>
+      <div className="flex flex-col">
+      <div className=" w-110 h-75 relative">
+      <div className="inset-0 absolute" style={{backgroundImage: `url(${apple3})`, borderRadius: 20, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
+      </div>
+      <div className="text-xs max-w-90 p-4">The Vitals app. Your daily health status, ASAP. Quickly see your overnight health data — including heart rate, respiratory rate, wrist temperature, blood oxygen and sleep duration — and get notified if multiple metrics are ever outside your typical range.6</div>
+      </div>
+      <div className="flex flex-col">
+      <div className=" w-110 h-75 relative">
+      <div className="inset-0 absolute" style={{backgroundImage: `url(${apple4})`, borderRadius: 20, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
+      </div>
+      <div className="text-xs max-w-90 p-4">Follow your heart on the ECG app. With Series 11 you can check for signs of atrial fibrillation, or AF, right from your wrist by using the ECG app to generate a single‑lead electrocardiogram.12</div>
+      </div>
+      <div className="flex flex-col">
+      <div className=" w-110 h-75 relative">
+      <div className="inset-0 absolute" style={{backgroundImage: `url(${apple5})`, borderRadius: 20, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
+      </div>
+      <div className="text-xs max-w-90 p-4">Stay on the pulse with the Heart Rate app. You can check your heart rate at any time — and get automatic notifications for a high or low heart rate or an irregular rhythm.13</div>
+      </div>
+      <div className="flex flex-col">
+      <div className=" w-110 h-75 relative">
+      <div className="inset-0 absolute" style={{backgroundImage: `url(${apple6})`, borderRadius: 20, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
+      </div>
+      <div className="text-xs max-w-90 p-4">Cycle Tracking keeps you in the loop. Since cycles vary, the best way to understand what’s normal for you is by tracking your menstrual cycle. You can use Cycle Tracking to log your period, record symptoms and get predictions about your next period or fertile window.14 It can also notify you if a cycle deviation is detected.</div>
       </div>
       </div>
-      <div className="mx-auto max-w-2xl" style={{textAlign: "center"}}>
+      
+      
+      </div>
+      </div>
+      </div>
+      <div className="mx-auto max-w-2xl mt-30" style={{textAlign: "center"}}>
       <div className="text-2xl text-[#fa1858]">Fitness</div>
-      <div className="text-6xl mt-5 mb-10">Better by every measure.</div>
+      <div ref={trigger2} className="text-6xl mt-5 mb-10">Better by every measure.</div>
       <div className="text-xl mb-10">Whether you’re training for a marathon or swimming lengths, Apple Watch Series 11 serves up endless ways to work out. And it uses helpful metrics to count every move, so you know how to push harder and train smarter. Let’s go — the starting line is on your wrist.</div>
+      </div>
+      <div>
+       <div className="flex">
+       <div style={{backgroundImage: `url(${watch1})`, width: 600, height: 600, backgroundSize: "contain", backgroundRepeat: "no-repeat" }}></div>
+       <div className="flex flex-col">
+       <div style={{backgroundImage: `url(${run})`, width: 50, height: 50, backgroundSize: "contain", backgroundRepeat: "no-repeat" }}></div>
+       <div className="text-xs w-60 pt-2">A refreshed Workout app. An improved layout highlights four new corner buttons, making it even quicker to access your go‑to features like Pacer, Race Route and Custom Workout.</div>
+       </div>
+       </div>
+       <div className="flex">
+       <div className="flex flex-col">
+       <div style={{backgroundImage: `url(${clock})`, width: 50, height: 50, backgroundSize: "contain", backgroundRepeat: "no-repeat" }}></div>
+       <div className="text-xs w-60 pt-2">Run circles around your Activity rings. Move. Exercise. Stand. The Activity app can track your daily activity. You can easily pause your rings or adjust your goals for each day of the week.</div>
+       </div>
+       <div className="" style={{backgroundImage: `url(${watch2})`, width: 600, height: 600, backgroundSize: "contain", backgroundRepeat: "no-repeat" }}></div>
+      </div>
       </div>
       <div className="max-w-2xl mx-auto">
       <div className=" text-4xl font-bold">Apple Watch</div>
